c00bfe99759aae5d821bbf4692245d47d1093e5e
onlystar1991 <onlystar1991@hotmail.com>
2017-01-25 01:05:00 +0100

    csv export function issue fixed

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -32,22 +32,27 @@ class Bom < ApplicationRecord
 			end
 			
 			printed_part_category = ""
-			changed = true; i = 0
+			changed = true
+			headers = Hash.new
+			final_outputs = Hash.new
 			outputs.each do |key, output|
 				if printed_part_category.eql? output[0].part.sub_category.part_category.name
 					changed = false
 				else
-					csv << [output[0].part.sub_category.part_category.name]
 					printed_part_category = output[0].part.sub_category.part_category.name
-					changed = true; i = 1;
+					changed = true
 				end
-				header = ["Part Name", "Part Description", "Manufacturer Part Number", "Qty", "Created At"]
+
+				header = ["Part Name", "Part Description", "Manufacturer Part Number", "Created At"]
 				row = []
 				row.push(output[0].part.part_name)
 				row.push(output[0].part.part_description)
 				row.push(output[0].part.number.empty? ? "" : output[0].part.number)
 				row.push(output[0].part.created_at.to_date)
 				output.each do | part_item |
+					if header.include? "Cost@#{part_item.count}"
+						next
+					end
 					if part_item.count > 2499
 						per_piece_price = part_item.part.price_2500.to_f
 					elsif part_item.count > 999
@@ -63,17 +68,22 @@ class Bom < ApplicationRecord
 					row.push(per_piece_price)
 					header.push("Cost@#{part_item.count}")
 				end
+				headers[output[0].part.sub_category.part_category.name] = header
 
-				rows = []
-				if changed and i == 1
-					csv << header
-					rows.each do |item|
-						csv << item
-					end
+				if final_outputs.has_key? output[0].part.sub_category.part_category.name
+					final_outputs[output[0].part.sub_category.part_category.name].push(row)
 				else
-					rows.push(row)
+					final_outputs[output[0].part.sub_category.part_category.name] = []
+					final_outputs[output[0].part.sub_category.part_category.name].push(row)
 				end
-				
+			end
+
+			final_outputs.each do |key, final_result|
+				csv << [key]
+				csv << headers[key]
+				final_result.each do |part|
+					csv << part
+				end
 			end
 		end
 	end
